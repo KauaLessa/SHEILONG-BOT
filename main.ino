d509a6db766286f8ed9d7b2b8f7cf529a25ac14a
Kauã Lessa <kauajoestar@gmail.com>
2025-10-20 13:55:10 -0300

    initial commit

---
+++ main.ino
@@ -0,0 +1,17 @@
+// Pinagem
+
+#define M1 = A7
+#define M2 = A6
+#define C1 = D2
+#define C2 = D3
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
