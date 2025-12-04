f254e46fd8d10e925fe93b1fe2bca04c88c4842b
Kauã Lessa <kauajoestar@gmail.com>
2025-12-04 08:38:49 -0300

    add pwm

--- main.ino
+++ main.ino
@@ -5,10 +5,12 @@ const int DETECTION_LIMIT = 80;
 // Motor direito
 const int RIGHT_MOTOR_INT1_PIN = 4; 
 const int RIGHT_MOTOR_INT2_PIN = 7; 
+const int PWMA = 5; 
 
 // Motor esquerdo
 const int LEFT_MOTOR_INT3_PIN = 8; 
 const int LEFT_MOTOR_INT4_PIN = 9; 
+const int PWMB = 6; // PWM esquerdo
 
 // Sensores de linha
 const int RIGHT_LINE_SENSOR_PIN = 2; 
@@ -26,49 +28,54 @@ const int OBS4_SENSOR_PIN = A6; // Esquerda
 
 typedef struct MOTOR {
   uint8_t IN1; 
-  uint8_t IN2; 
+  uint8_t IN2;
+  uint8_t PWM; 
 } MOTOR; 
 
-MOTOR init_motor(uint8_t IN1, uint8_t IN2) {
+MOTOR init_motor(uint8_t IN1, uint8_t IN2, uint8_t PWM) {
   MOTOR motor; 
   motor.IN1 = IN1; 
-  motor.IN2 = IN2; 
+  motor.IN2 = IN2;
+  motor.PWM = PWM; 
   pinMode(IN1, OUTPUT); 
   pinMode(IN2, OUTPUT); 
+  pinMode(PWM, OUTPUT); 
   return motor; 
 }
 
-void motor_foward(MOTOR motor) {
+void motor_foward(MOTOR motor, int speed) {
   digitalWrite(motor.IN1, HIGH); 
   digitalWrite(motor.IN2, LOW); 
+  analogWrite(motor.PWM, speed); 
 }
 
-void motor_back(MOTOR motor) {
+void motor_back(MOTOR motor, int speed) {
   digitalWrite(motor.IN1, LOW); 
   digitalWrite(motor.IN2, HIGH); 
+  analogWrite(motor.PWM, speed); 
 }
 
-MOTOR right_motor = init_motor(RIGHT_MOTOR_INT1_PIN, RIGHT_MOTOR_INT2_PIN); 
-MOTOR left_motor = init_motor(LEFT_MOTOR_INT3_PIN, LEFT_MOTOR_INT4_PIN); 
+MOTOR right_motor = init_motor(RIGHT_MOTOR_INT1_PIN, RIGHT_MOTOR_INT2_PIN, PWMA); 
+MOTOR left_motor = init_motor(LEFT_MOTOR_INT3_PIN, LEFT_MOTOR_INT4_PIN, PWMB); 
 
-void move_robot_foward() {
-  motor_foward(right_motor); 
-  motor_foward(left_motor); 
+void move_robot_foward(int speed) {
+  motor_foward(right_motor, speed); 
+  motor_foward(left_motor, speed); 
 }
 
-void move_robot_backward() {
-  motor_back(right_motor); 
-  motor_back(left_motor); 
+void move_robot_backward(int speed) {
+  motor_back(right_motor, speed); 
+  motor_back(left_motor, speed); 
 }
 
-void turn_left() {
-  motor_foward(right_motor); 
-  motor_back(left_motor); 
+void turn_left(int speed) {
+  motor_foward(right_motor, speed); 
+  motor_back(left_motor, speed); 
 }
 
-void turn_right() {
-  motor_back(right_motor); 
-  motor_foward(left_motor); 
+void turn_right(int speed) {
+  motor_back(right_motor, speed); 
+  motor_foward(left_motor, speed); 
 }
 
 void setup() {
@@ -84,7 +91,6 @@ int getDistance(int sensor_pin) {
   return distanceCM; 
 }
 
-
 void sens_obs() {
   int front_right = getDistance(OBS1_SENSOR_PIN); 
   int front_left  = getDistance(OBS2_SENSOR_PIN); 
@@ -94,35 +100,35 @@ void sens_obs() {
   // 1. Ataque direto (inimigo na frente)
   if (front_right <= DETECTION_LIMIT && front_left <= DETECTION_LIMIT) {
     Serial.println("ATAQUE!!!");
-    move_robot_foward(); 
+    move_robot_foward(255); 
   }
 
-  // 2. Alinhamento lateral
-  else if (side_right <= DETECTION_LIMIT) {
-    Serial.println("Virando pra direita (inimigo lateral)...");
-    turn_right();
-  }
-
-  else if (side_left <= DETECTION_LIMIT) {
-    Serial.println("Virando pra esquerda (inimigo lateral)...");
-    turn_left();
-  }
-
-  // 3. Alinhamento frontal
+   // 2. Alinhamento frontal
   else if (front_right <= DETECTION_LIMIT) {
     Serial.println("Ajuste direita...");
-    turn_right();
+    turn_right(255);
   }
 
   else if (front_left <= DETECTION_LIMIT) {
     Serial.println("Ajuste esquerda...");
-    turn_left();
+    turn_left(255);
+  }
+
+  // 3. Alinhamento lateral
+  else if (side_right <= DETECTION_LIMIT) {
+    Serial.println("Virando pra direita (inimigo lateral)...");
+    turn_right(255);
+  }
+
+  else if (side_left <= DETECTION_LIMIT) {
+    Serial.println("Virando pra esquerda (inimigo lateral)...");
+    turn_left(255);
   }
 
   // 4. Nenhum sensor detectou → procurar
   else {
     Serial.println("Procurando...");
-    move_robot_foward();
+    move_robot_foward(126);
   }
 }
 
@@ -130,19 +136,19 @@ void loop() {
   sens_obs(); 
 
   if(digitalRead(LEFT_LINE_SENSOR_PIN) == LOW) {
-    move_robot_backward(); 
+    move_robot_backward(255); 
     Serial.println("tras..."); 
     delay(200); 
-    turn_right(); 
+    turn_right(255); 
     Serial.println("Direita..."); 
     delay(250); 
   }
 
   if(digitalRead(RIGHT_LINE_SENSOR_PIN) == LOW) {
-    move_robot_backward(); 
+    move_robot_backward(255); 
     Serial.println("tras..."); 
     delay(200); 
-    turn_left(); 
+    turn_left(255); 
     Serial.println("Esquerda..."); 
     delay(250); 
   }
